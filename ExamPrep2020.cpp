7a11c5b2de3c9d1aebed96b3e5f84ef0481202a1
Hans Ola Hoftun <hans_olahoftun@hotmail.com>
2020-11-17 12:48:13 +0100

    Add project files.

---
+++ ExamPrep2020.cpp
@@ -0,0 +1,92 @@
+
+#include <iostream>
+#include "Header.h"
+
+int main()
+{
+    if (UnlockingMechanism())
+    {
+        std::cout << "Unlocked! Welcome\n";
+    }
+}
+
+bool UnlockingMechanism()
+{
+    int horizontal = 0;
+    std::string current_password;
+    PrintLock();
+    do 
+    {
+        switch (_getch())
+        {
+        case KEY_UP :
+            if (!((int)_Lock[1][horizontal] + 1 > 90))
+            {
+                _Lock[1][horizontal] = ((int)_Lock[1][horizontal] + 1);
+                PrintLock();
+            }
+            break;
+		case KEY_DOWN:
+			if (!((int)_Lock[1][horizontal] -1 < 65))
+			{
+				_Lock[1][horizontal] = ((int)_Lock[1][horizontal] - 1);
+				PrintLock();
+			}
+			break;
+		case KEY_LEFT:
+            if (!(horizontal - 1 < 0))
+            {
+                _Lock[0][horizontal] = ' ';
+                horizontal--;
+                _Lock[0][horizontal] = 'V';
+                PrintLock();
+            }
+			break;
+		case KEY_RIGHT:
+			if (!(horizontal+ 1 >= SIZE))
+			{
+                _Lock[0][horizontal] = ' ';
+				horizontal++;
+                _Lock[0][horizontal] = 'V';
+
+                //This is to initialize the spot if its the first time you have moved there
+				if (_Lock[1][horizontal] == '*')
+				{
+                    _Lock[1][horizontal] = 65;
+				}
+                PrintLock();
+			}
+			break;
+        default:
+            break;
+        }
+		current_password = "";
+		for (int x = 0; x < SIZE; x++)
+		{
+			current_password.push_back(_Lock[1][x]);
+		}
+		if (current_password == password)
+		{
+            return true;
+		}
+    } while (true);
+}
+
+void PrintLock()
+{
+    system("CLS");
+
+    for (int y = 0; y < 2 ; y++)
+    {
+        std::cout << "\t\t";
+		for (int x = 0; x < SIZE; x++)
+		{
+            std::cout << "[" << _Lock[y][x] << "]";
+		}
+        std::cout << std::endl;
+    }
+}
+void PasswordCheck()
+{
+    
+}
---
+++ Header.h
@@ -0,0 +1,18 @@
+#pragma once
+#define SIZE 6
+
+#define KEY_DOWN 80
+#define KEY_UP 72
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
+#include <conio.h>
+#include <string>
+
+
+char _Lock[2][6]{ {'V',' ',' ',' ',' ',' '},{'A','*','*','*','*','*'} };
+
+std::string password{ "ABCDEF" };
+bool UnlockingMechanism();
+void PrintLock();
+void PasswordCheck();
